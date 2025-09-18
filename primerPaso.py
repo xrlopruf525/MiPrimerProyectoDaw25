873bde05afea01e29b1df50a8357433af4ad04b8
xrlopruf525 <xrlopruf525@ieshnosmachado.org>
2025-09-18 09:49:24 +0200

    Mi primer programa

--- primerPaso.py
+++ primerPaso.py
@@ -1,1 +1,4 @@
-print ("Hola me llamo Ruben")
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+
+print ("El resultado de la suma es: ", num1 + num2)
