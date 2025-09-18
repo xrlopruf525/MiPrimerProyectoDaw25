382e3e5556fc777a1d0de98137150db994dd0fd3
xrlopruf525 <xrlopruf525@ieshnosmachado.org>
2025-09-18 10:29:52 +0200

    Print bien hechos

--- primerPaso.py
+++ primerPaso.py
@@ -1,4 +1,9 @@
-num1 = int(input("Ingrese el primer número: "))
-num2 = int(input("Ingrese el segundo número: "))
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
 
-print ("El resultado de la suma es: ", num1 + num2)
+print (f"El resultado de la suma es: {num1+num2}")
+print (f"El resultado de la resta es: {num1-num2}")
+print (f"El resultado de la multiplicación es: {num1*num2}")
+print (f"El resultado de la división es: {num1/num2}")
+
+
